3f0ef3d2cc89dc56d911e5986d1b94e0d6312a51
jczimm <jczimm.alias@gmail.com>
2014-04-15 09:34:21 -0400

    Update makefile.php

--- makefile.php
+++ makefile.php
@@ -6,12 +6,25 @@
   </head>
   <body>
     <?php
-      $file = 'log.txt';
+      /*$file = 'log.txt';
       $handle = fopen($file, 'w') or die('Cannot open file:  '.$file);
       $data = htmlspecialchars($_POST["data"]);
       fwrite($handle, $data);
       
-      readfile($handle); // download
+      readfile($handle); // download*/
+      
+      $handle = fopen("log.txt", "w") or die("Cannot open file");
+      fwrite($handle, $_POST["data"]);
+      fclose($handle);
+  
+      header('Content-Type: application/octet-stream');
+      header('Content-Disposition: attachment; filename='.basename('log.txt'));
+      header('Expires: 0');
+      header('Cache-Control: must-revalidate');
+      header('Pragma: public');
+      header('Content-Length: ' . filesize('log.txt'));
+      readfile('log.txt');
+      exit;
     ?>
   </body>
 </html>
