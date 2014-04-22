b1b590d1fc5d3478ec5b9942610d2df6b52f228f
jczimm <jczimm.alias@gmail.com>
2014-04-22 18:46:01 -0400

    Update script.js

--- psi-assets/script.js
+++ psi-assets/script.js
@@ -1,5 +1,5 @@
 $(function(){
-  var $playerData = $("player").tubeplayer("data");
+  var $playerData = $("#player").tubeplayer("data");
 
   while($playerData.bytesLoaded !== $playerData.bytesTotal){}
   alert("done!");
