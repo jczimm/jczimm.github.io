f8ddadc28d8b8ddd5edc475e93bd996d37297c23
jczimm <jczimm.alias@gmail.com>
2014-04-22 18:34:10 -0400

    Update script.js

--- psi-assets/script.js
+++ psi-assets/script.js
@@ -1,3 +1,18 @@
 $(function(){
+  var ytplayer;
+
+  function onYouTubePlayerReady(playerId) {
+      ytplayer = document.getElementById("myytplayer");
+      checkBuffer();
+  }
   
+  function checkBuffer(){
+      if(ytplayer.getVideoBytesLoaded() == ytplayer.getVideoBytesTotal()){
+          alert('Buffer Complete!');
+      }else{
+          var t = setTimeout(function(){
+              Editor.split();
+          },1000);
+      }
+  }
 });
