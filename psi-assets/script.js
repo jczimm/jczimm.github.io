1fbca8f47070b414d353f23573b454b8976f1f76
jczimm <jczimm.alias@gmail.com>
2014-04-22 19:13:41 -0400

    Update script.js

--- psi-assets/script.js
+++ psi-assets/script.js
@@ -1,3 +1,8 @@
-$(function(){
-  
+$(document).ready(function(){
+  $("#player").attr("width",$(window).width());
+  $("#player").attr("height",$(window).height());
+  $(document).resize(function(){
+    $("#player").attr("width",$(window).width());
+    $("#player").attr("height",$(window).height());
+  });
 });
