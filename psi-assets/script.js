578de3bb552a1db43b4c570dceadb44c74743d5a
jczimm <jczimm.alias@gmail.com>
2014-04-22 19:56:55 -0400

    Update script.js

--- psi-assets/script.js
+++ psi-assets/script.js
@@ -1,33 +1,3 @@
 $(document).ready(function(){
-  var video = document.getElementById("player");
-  video.pause();
   
-  $("#player").attr("width",$(window).width());
-  $("#player").attr("height",$(window).height());
-  $(document).resize(function(){
-    $("#player").attr("width",$(window).width());
-    $("#player").attr("height",$(window).height());
-  });
-  
-  var playing = false, paused = true;
-  
-  $(document).keypress(function(e){
-    switch(e.keyCode){
-      case 32: // space
-        if (playing) {
-          if (paused) {
-            $("#pause-icon").fadeOut();
-            paused = false;
-            video.play();
-          } else {
-            $("#pause-icon").fadeIn(400, function () {
-              $("#pause-icon").finish();
-            });
-            paused = true;
-            video.pause();
-          }
-        }
-        break;
-      }
-  });
 });
