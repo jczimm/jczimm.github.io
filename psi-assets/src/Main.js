3168e38a77fb0b707dd2f87c28ae2805843527b3
jczimm <jczimm.alias@gmail.com>
2014-05-08 08:54:28 -0400

    Update Main.js

--- psi-assets/src/Main.js
+++ psi-assets/src/Main.js
@@ -489,12 +489,11 @@ function SplashController( game, camera, scene, isGameover ) {
 
     this.counter = 0;
 
-    var text = new THREE.TextGeometry( "      PSI",
+    var text = new THREE.TextGeometry( "PSI",
         {
-            size: 6, height: 4, curveSegments: 4,
+            size: 10, height: 4, curveSegments: 4,
             font: "helvetiker", style: "normal"
         });
-    //window.main.loader.get("psi-assets/assets/models/tessitron_text.js");
     var logo = window.main.loader.get("psi-assets/assets/models/tessitron_logo.js");
     var tmat = new THREE.MeshPhongMaterial( { color: 0xffffff, shading: THREE.FlatShading  } );
     var lmat = new THREE.MeshPhongMaterial( { color: 0xf2e85c, shading: THREE.FlatShading  } );
@@ -503,7 +502,7 @@ function SplashController( game, camera, scene, isGameover ) {
     this.lm.rotation. x = Math.PI / 2;
 
     this.tm = new THREE.Mesh(text, tmat);
-    this.tm.position.x = -65;
+    this.tm.position.x = -10;
     this.tm.scale = new THREE.Vector3(2,2,2);
 
     this.textMesh = new THREE.Object3D();
