f4624985c93bc3201207a5367b3f658b8cce40d4
hwstack <jczimm.alias@gmail.com>
2014-05-10 20:21:53 -0400

    commit

--- psi-assets/src/level/Level.js
+++ psi-assets/src/level/Level.js
@@ -21,6 +21,7 @@ Level.prototype.update = function(dt) {
 
 Level.prototype.build = function() {
     this.chunks = [
+		new BossChunk1(this),
         new IntroChunk(this),
         new SparseChunk(this),
         new FourthNoteChunk(this),
