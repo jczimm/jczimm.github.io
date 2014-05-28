2cec4172d2e89ece0b8a6f8eae2df897f6e93c60
mytasks <platypusnetwork@gmail.com>
2014-05-28 08:57:30 -0400

    commit wooooooo

--- chi/main.js
+++ chi/main.js
@@ -7,14 +7,22 @@ var main_state = {
 
     preload: function() {
         // Everything in this function will be executed at the beginning. That’s where we usually load the game’s assets (images, sounds, etc.)
+		// Load a sprite in the game
+		// Parameters: name of the sprite, path to the image
+		game.load.image('hello', 'assets/hello.png'); 
     },
 
     create: function() { 
         // This function will be called after the preload function. Here we set up the game, display sprites, add labels, etc.
+		// Display a sprite on the screen
+		// Parameters: x position, y position, name of the sprite
+		this.hello_sprite = game.add.sprite(250, 300, 'hello');  
     },
 
     update: function() {
         // This is where we will spend the most of our time. This function is called 60 times per second to update the game.
+		// Increase the angle of the sprite by one degree
+		this.hello_sprite.angle += 1;  
     } 
 }
 
