8856eb5fc15eaa7d5c643ea719168aa837127557
hwstack <jczimm.alias@gmail.com>
2014-05-28 20:50:09 -0400

    committing stuff ftw

--- chi/main.js
+++ chi/main.js
@@ -1,6 +1,6 @@
 // We start by initializing Phaser
 // Parameters: width of the game, height of the game, how to render the game, the HTML div that will contain the game
-var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, 'game_div');
+var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.WEBGL, 'game_div');
 
 // And now we define our first and only state, I'll call it 'main'. A state is a specific scene of a game like a menu, a game over screen, etc.
 var main_state = {
@@ -20,32 +20,37 @@ var main_state = {
         // This function will be called after the preload function. Here we set up the game, display sprites, add labels, etc.
 		// Display a sprite on the screen
 		// Parameters: x position, y position, name of the sprite
-		this.hello_sprite = game.add.sprite(width/2, height/2, 'hello');  
+		this.hello_sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'hello');  
     },
 
     update: function() {
         // This is where we will spend the most of our time. This function is called 60 times per second to update the game.
 		// Increase the angle of the sprite by one degree
-		this.hello_sprite.x = width/2;
-		this.hello_sprite.x = height/2;
+		this.hello_sprite.x = game.world.centerX;
+		this.hello_sprite.y = game.world.centerY;
 		this.hello_sprite.angle += 1;  
     } 
 }
 
 var display = {
     resize: function () {
-		window.height = $(window).innerWidth();
-		window.width = $(window).innerHeight();
+		window.height = $(window).innerHeight();
+		window.width = $(window).innerWidth();
         try {
             game.width = Number(width);
             game.height = Number(height);
             game.stage.bounds.width = Number(width);
             game.stage.bounds.height = Number(height);
             game.renderer.resize(Number(width), Number(height));
+			game.world.setBounds(0, 0, width, height);
+			game.camera.setSize(width, height);
+			game.camera.setBoundsToWorld();
+			if (game.renderType === Phaser.WEBGL) {
+			  game.renderer.resize(width, height);    
+			}
         } catch (e) {
             console.log("Error: "+e.message);
         }
-
     }
 
 };
