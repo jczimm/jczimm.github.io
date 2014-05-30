93923cb9592ecd82b43323f1c0c52f4ca59a05db
mytasks <platypusnetwork@gmail.com>
2014-05-30 16:56:12 -0400

    commit wooooooo

--- chat/client/connect.js
+++ chat/client/connect.js
@@ -11,9 +11,3 @@ function Connection(ip,port){
 }
 
 var IP = prompt("connect to ip:"), Main = new Connection(IP,'8888');
-
-function send(m){
-	Main.socket.send(JSON.stringify({msg:m, time:new Date()}));
-}
-
-var Main = new Connection('192.168.1.131','8888');
