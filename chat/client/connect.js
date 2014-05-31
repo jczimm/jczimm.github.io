44505ffc7e46ad587a148916218b4c54d0d4da2c
hwstack <jczimm.alias@gmail.com>
2014-05-31 16:46:17 -0400

    mhmmm

--- chat/client/connect.js
+++ chat/client/connect.js
@@ -8,13 +8,7 @@ function Connection(ip,port){
 	this.socket.onmessage = function(m){
 		console.log(m.data);
 	}
-	this.socket.send(JSON.stringify(new Message(USERNAME, "userJoin", undefined, "transmission")));
 }
-
-var USERNAME;
-do {
-	USERNAME = prompt("choose a username");
-} while(USERNAME === undefined);
 
 var IP, ipRegexp = /^(?:(?:2[0-4]\d|25[0-5]|1\d{2}|[1-9]?\d)\.){3}(?:2[0-4]\d|25[0-5]|1\d{2}|[1-9]?\d)$/;
 if(location.hash.replace("#","").match(ipRegexp)) IP = location.hash.replace("#","");
@@ -23,5 +17,11 @@ else {
 	while(!IP.match(ipRegexp)) IP = prompt("invalid ip");
 }
 
-var Main = new Connection(IP,'8888');
+var Main = new Connection(IP,'8888'),
+	USERNAME;
+do {
+	USERNAME = prompt("choose a username");
+} while(USERNAME === undefined);
+Main.socket.send(JSON.stringify(new Message(USERNAME, "userJoin", undefined, "transmission")));
+
 initDisplay();
