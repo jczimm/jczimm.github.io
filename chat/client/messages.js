cffe82729e6355d839ea43a0b6df5d545eb6a510
hwstack <jczimm.alias@gmail.com>
2014-05-30 20:36:25 -0400

    mhmmm

--- chat/client/messages.js
+++ chat/client/messages.js
@@ -4,13 +4,18 @@ function Message(user,msg,time){
 	this.time = time || new Date();
 }
 
+var msgs = [];
 function send(u,m){
-	Main.socket.send(JSON.stringify(new Message(USERNAME, m)));
+	var msg = new Message(USERNAME, m));
+	Main.socket.send(JSON.stringify(msg));
+	msgs.push(msg);
 }
 
+var lm;
 function initDisplay(){
 	Main.socket.onmessage = function(m){
 		var data = JSON.parse(m.data);
-		$("#messages").prepend("<tr class='line'><td class='user'>"+ data.user +"</td><td class='msg'>" + data.msg + "</td><td class='time'>" + data.time +"</td></tr>");
+		$("#messages").prepend("<tr class='line'><td class='user'>"+ lm.user === data.user ? "" : data.user +"</td><td class='msg'>" + data.msg + "</td><td class='time'>" + data.time +"</td></tr>");
+		lm = data;
 	}
 }
