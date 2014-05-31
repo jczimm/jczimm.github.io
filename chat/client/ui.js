a3c6ed989904b1500c1f1beedd120a78f452392f
hwstack <jczimm.alias@gmail.com>
2014-05-30 21:55:45 -0400

    mhmmm

--- chat/client/ui.js
+++ chat/client/ui.js
@@ -1,10 +1,1 @@
-(function($) {
-    $.fn.onEnter = function(func) {
-        this.bind('keypress', function(e) {
-            if (e.keyCode === 13) func();
-        });
-        return this; 
-     };
-})(jQuery);
-
-$("#msg-box").onEnter(submit);
+$("#msg-box").keypress(function(e){ if (e.keyCode === 13) submit(); });
