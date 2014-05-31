a47a52a90b939470d19ecca59af4a07aa3b81e29
hwstack <jczimm.alias@gmail.com>
2014-05-30 22:26:38 -0400

    mhmmm

--- chat/client/messages.js
+++ chat/client/messages.js
@@ -2,6 +2,7 @@ function Message(user,msg,time){
 	this.user = user;
 	this.msg = msg;
 	this.time = time || new Date();
+	this.timeago = moment(this.time).fromNow();
 }
 
 function submit(){
@@ -22,7 +23,9 @@ function initDisplay(){
 	Main.socket.onmessage = function(m){
 		var data = JSON.parse(m.data);
 		var userToDisplay = lm.user === data.user ? "" : data.user === USERNAME ? "<b>me</b>" : data.user;
-		$("#messages").prepend("<tr class='line'><td class='user'>"+userToDisplay+"</td><td class='msg'>"+data.msg+"</td><td class='time'>"+data.time+"</td></tr>");
+		$("#messages").prepend("<tr class='line'><td class='user'>"+userToDisplay+"</td><td class='msg'>"+data.msg+"</td><td class='time' alt='"+data.time+"'>"+data.timeago+"</td></tr>");
 		lm = data;
 	}
 }
+
+//TODO: update `$(".time")`'s text for its `timeago` every minute or so (use `msgs` array)
