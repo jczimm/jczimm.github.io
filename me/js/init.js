1b4b507f7a7d0a0bb157459abbdd1d359c1ca4ba
hwstack <jczimm.alias@gmail.com>
2014-07-09 19:31:14 -0400

    cleanup

--- me/js/init.js
+++ me/js/init.js
@@ -8,11 +8,11 @@
 	skel.init({
 		reset: 'full',
 		breakpoints: {
-			'global': { range: '*', href: 'css/style.css', lockViewport: true, viewport: 'minimal-ui' },
-			'wide': { range: '-1680', href: 'css/style-wide.css' },
-			'normal': { range: '-1280', href: 'css/style-normal.css' },
-			'mobile': { range: '-640', href: 'css/style-mobile.css' },
-			'mobilep': { range: '-360', href: 'css/style-mobilep.css' }
+			'global': { range: '*', href: 'me/css/style.css', lockViewport: true, viewport: 'minimal-ui' },
+			'wide': { range: '-1680', href: 'me/css/style-wide.css' },
+			'normal': { range: '-1280', href: 'me/css/style-normal.css' },
+			'mobile': { range: '-640', href: 'me/css/style-mobile.css' },
+			'mobilep': { range: '-360', href: 'me/css/style-mobilep.css' }
 		}
 	});
 
