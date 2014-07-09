652a9a7c77d0f4b9d48499f56c43df783b9524ef
hwstack <jczimm.alias@gmail.com>
2014-07-09 19:34:04 -0400

    cleanup

--- me/js/init.js
+++ me/js/init.js
@@ -21,17 +21,20 @@
 	// Remove "loading" class once the page has fully loaded.
 		window.onload = function() {
 			document.body.className = '';
-		}
+		};
 
 	// Prevent scrolling on touch.
 		window.ontouchmove = function() {
 			return false;
-		}
+		};
 
 	// Fix scroll position on orientation change.
 		window.onorientationchange = function() {
 			document.body.scrollTop = 0;
-		}
+		};
+
+	// Catch all errors
+		window.onerror = function(){};
 
 /*
 
