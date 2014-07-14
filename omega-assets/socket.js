4fad71facc9238c0e97ce97f6e61642af71a6bc9
jczimm <jczimm.alias@gmail.com>
2014-07-14 14:51:56 -0400

    Update socket.js

--- omega-assets/socket.js
+++ omega-assets/socket.js
@@ -2,4 +2,5 @@ var pusher = new Pusher('6bd9ea172357cea30784');
 var channel = pusher.subscribe('leaderboard');
 channel.bind('new_highscore', function (data) {
     var data = JSON.parse(data);
+    console.log(data);
 });
