633b1628534c1babda4644e62a404d191907ee60
jczimm <jczimm.alias@gmail.com>
2014-07-14 16:10:59 -0400

    Update pusher_auth.php

--- omega-assets/pusher_auth.php
+++ omega-assets/pusher_auth.php
@@ -1,12 +1,16 @@
 <?php
 
-require('Pusher.php');
+require_once('Pusher.php');
 
 $pusher = new Pusher(
     '6bd9ea172357cea30784', //APP KEY
     '7736700fdb75b48640ce', //APP SECRET
     '81508' //APP ID
 );
+
+
+session_start();
+header('Content-Type: application/json');
 
 $auth = $pusher->socket_auth($_POST['channel_name'], $_POST['socket_id']);
 
