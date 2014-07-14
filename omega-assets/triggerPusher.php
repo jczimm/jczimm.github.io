7e66ea624cfd349779706f4c2364182d804961fe
jczimm <jczimm.alias@gmail.com>
2014-07-14 15:11:46 -0400

    Create triggerPusher.php

---
+++ omega-assets/triggerPusher.php
@@ -0,0 +1,29 @@
+<?php
+
+//include the pusher publisher library
+include_once 'Pusher.php';
+ 
+$pusher = new Pusher(
+    '6bd9ea172357cea30784', //APP KEY
+    '7736700fdb75b48640ce', //APP SECRET
+    '81508' //APP ID
+);
+ 
+//get the message posted by our ajax call
+$message = $_POST['message'];
+ 
+//trigger the 'new_message' event in our channel, 'presence-nettuts'
+$pusher->trigger(
+    'leaderboard', //the channel
+    'new_highscore', //the event
+    array('message' => $message) //the data to send
+);
+ 
+//echo the success array for the ajax call
+echo json_encode(array(
+    'message' => $message,
+    'success' => true
+));
+exit();
+
+?>
