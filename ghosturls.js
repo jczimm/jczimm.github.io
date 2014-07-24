98b1759de05cd3f6d89675c994596d62ac6404ac
jczimm <jczimm.alias@gmail.com>
2014-07-24 11:03:30 -0400

    Update ghosturls.js

--- ghosturls.js
+++ ghosturls.js
@@ -1,7 +1,11 @@
-switch(location.href.substr(17)){
-  var urls = [
+var urls = [
   
-    ["/cdn", "http://cdn.jczimm.com"]
-    
-  ];
-}
+  ["/cdn", "http://cdn.jczimm.com"],
+  ["/blog", "http://blog.jczimm.com"]
+  
+];
+
+urls.forEach(function(pair){
+  if(location.href.substr(17) === pair[0])
+    location.href = pair[1];
+});
