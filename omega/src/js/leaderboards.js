8c59bd1f41a363f49330e2253b24231310e2c6d1
mytasks <platypusnetwork@gmail.com>
2014-08-03 14:19:54 -0400

    Try again

--- omega/src/js/leaderboards.js
+++ omega/src/js/leaderboards.js
@@ -2,9 +2,6 @@ var leaderboardServer = new WebSocket('ws://underground.jczimm.com');
 
 leaderboardServer.onopen = function(){
 	console.log('.: Connected to the leaderboard server :.');
-	setInterval(function(){
-		leaderboardServer.send("keepalive");
-    }, 3000);
 };
 
 leaderboardServer.onclose = function(){
