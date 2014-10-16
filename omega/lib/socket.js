b680b531d488fd3c2c41e9a386146ab9f2c6a796
mytasks <platypusnetwork@gmail.com>
2014-10-16 19:57:53 -0400

    moved omega

--- omega/lib/socket.js
+++
@@ -1,9 +0,0 @@
-var pusher = new Pusher('6bd9ea172357cea30784', { authEndpoint: 'omega-assets/pusher_auth.php' });
-var channel = pusher.subscribe('private-leaderboard');
-channel.bind('new_highscore', function (data) {
-    console.log(data);
-});
-
-channel.bind('pusher:subscription_succeeded', function() {
-    var triggered = channel.trigger('client-test', { "foo": "bar" });
-});
